5132d3a1aec28771e9abfb245081325eea95cfee
saadetsimsek <saadetsimsek13@gmail.com>
2025-02-25 23:47:47 +0300

    Initial Commit

---
+++ Card/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Card
+//
+//  Created by Saadet Şimşek on 25/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CardTests/CardTests.swift
@@ -0,0 +1,17 @@
+//
+//  CardTests.swift
+//  CardTests
+//
+//  Created by Saadet Şimşek on 25/02/2025.
+//
+
+import Testing
+@testable import Card
+
+struct CardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
